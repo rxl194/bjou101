43c36627a732855eff34a649be5cf5b381a8c270
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-10-01 16:01:55 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- cex/Ionic/LearnIonic/ionic-chat-app/www/js/app.js
+++ cex/Ionic/LearnIonic/ionic-chat-app/www/js/app.js
@@ -5,9 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('IonicChatApp', ['ionic', 'chatapp.controllers', 'chatapp.services', 'chatapp.directives', 
+  'ngCordova', 'ngCordovaOauth', 'firebase'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,66 +21,111 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('main');
+      }
+    });
+    
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  
+  $ionicConfigProvider.backButton.previousTitleText(false);
+  $ionicConfigProvider.views.transition('platform');
+  $ionicConfigProvider.navBar.alignTitle('center');  
+  
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+  .state('main', {
+    url: '/',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl',
+    cache: false,
+    resolve: {
+      'currentAuth': ['FBFactory', 'Loader', function(FBFactory, Loader) {
+        Loader.show('Checking Auth..');
+        return FBFactory.auth().$waitForSignIn();
+      }]
+    }
   })
-
-  // Each tab has its own nav history stack:
-
+        
+  .state('tab', {
+    url: "/tab",
+    abstract: true,
+    cache: false,
+    templateUrl: "templates/tabs.html"
+  })
+        
   .state('tab.dash', {
     url: '/dash',
+    cache: false,
     views: {
       'tab-dash': {
         templateUrl: 'templates/tab-dash.html',
         controller: 'DashCtrl'
       }
+    },
+    resolve: {
+      'currentAuth': ['FBFactory', function(FBFactory) {
+        return FBFactory.auth().$requireSignIn();
+      }]
     }
   })
-
+        
   .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
+    url: '/chats',
+    cache: false,
+    views: {
+      'tab-chats': {
+      templateUrl: 'templates/tab-chats.html',
+      controller: 'ChatsCtrl'
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
+    },
+    resolve: {
+      'currentAuth': ['FBFactory', function(FBFactory) {
+        return FBFactory.auth().$requireSignIn();
+      }]
+    }
+  })
+        
   .state('tab.account', {
     url: '/account',
+    cache: false,
     views: {
       'tab-account': {
         templateUrl: 'templates/tab-account.html',
         controller: 'AccountCtrl'
       }
+    },
+    resolve: {
+      'currentAuth': ['FBFactory', function(FBFactory) {
+        return FBFactory.auth().$requireSignIn();
+      }]
+    }
+  })
+  
+  .state('chat-detail', {
+    url: '/chats/:otherUser',
+    templateUrl: 'templates/chat-detail.html',
+    controller: 'ChatDetailCtrl',
+    cache: false,
+    resolve: {
+      'currentAuth': ['FBFactory', 'Loader', function(FBFactory, Loader) {
+        Loader.show('Checking Auth..');
+        return FBFactory.auth().$requireSignIn();
+      }]
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/');
 
 });
+
+
